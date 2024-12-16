9e2562ec5c0ee1f7266a5b0c005105d10d434e12
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-16 19:27:38 +0530

    Update downloadrepo.py

--- SHUKLAMUSIC/plugins/misc/downloadrepo.py
+++ SHUKLAMUSIC/plugins/misc/downloadrepo.py
@@ -1,7 +1,7 @@
 """**
 MIT License
 
-Copyright (c) [All] [STRANGER TEAM]
+Copyright (c) [All] [TEAM RIYA ]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
