5bb732ee3dcb7bc2e6b78df6db941b3cc7e2dfb5
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-16 17:19:36 +0530

    Update imposter.py

--- SHUKLAMUSIC/plugins/tools/imposter.py
+++ SHUKLAMUSIC/plugins/tools/imposter.py
@@ -30,8 +30,8 @@ ROY = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
-            url=f"https://t.me/Itz_SapnaMusicbot?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/SHIVANSH474")
+            url=f"https://t.me/riyaXmusicbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/KSD_BOT_NETWORK")
     ],
 ]
 
