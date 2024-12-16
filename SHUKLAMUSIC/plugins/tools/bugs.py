c6072d042ca6b4d0046ba8901ab005526f793910
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-16 17:35:11 +0530

    Update bugs.py

--- SHUKLAMUSIC/plugins/tools/bugs.py
+++ SHUKLAMUSIC/plugins/tools/bugs.py
@@ -71,7 +71,7 @@ async def bugs(_, msg: Message):
             )
             await app.send_photo(
                 -1001802990747,
-                photo="https://telegra.ph/file/f66e5843568d4b7f2a652.jpg",
+                photo="https://files.catbox.moe/7mwofd.jpg",
                 caption=f"{bug_report}",
                 reply_markup=InlineKeyboardMarkup(
                     [
